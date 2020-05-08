f34a6be5a964f4819856595e7de2cabbaf98bd39
Jasmine Lok <jasminellok@gmail.com>
2020-05-08 01:51:20 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,8 +1,5 @@
 // declare your functions here...
-$(document).ready(function() {
-  function paragraphSelector() {
-    return $("p");
-  }
 
-
-});
+function paragraphSelector() {
+  return $("p");
+}
